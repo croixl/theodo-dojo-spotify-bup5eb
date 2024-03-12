78b38757b08cfc759a12781b264066e2b6fa55cb
croixl <louis.croix@hotmail.fr>
2024-03-12 14:04:57 +0100

    modif accueil

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const App = () => {
         <h1 className="App-title">Bienvenue sur le blind test</h1>
       </header>
       <div className="App-images">
-        <p>Il va falloir modifier le code pour faire un vrai blind test !</p>
+        <p>Oh non, il y a du travail.. Il va falloir modifier le code pour faire un vrai blind test !</p>
       </div>
       <div className="App-buttons"></div>
     </div>
